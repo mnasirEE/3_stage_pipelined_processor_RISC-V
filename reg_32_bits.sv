a3dfc2970887067a3d65a885a16289863fdc154b
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2024-03-23 02:03:22 +0500

    32 bits register created

---
+++ reg_32_bits.sv
@@ -0,0 +1,27 @@
+// 
+module reg_32_bits #(
+    parameter data_width = 32
+) (
+    input logic [data_width - 1:0] data_in,
+    input logic clock,
+    input logic reset,
+    output logic [data_width - 1:0] data_out
+);
+// 32 bit register
+reg [data_width - 1:0] register_32_bits;
+
+always_ff @( posedge clock, posedge reset ) begin : 32bits_register
+    if (reset) begin
+        register_32_bits <= 32'b0;
+    end
+    else begin
+        register_32_bits <= data_in;
+        data_out <= register_32_bits
+    end
+end
+
+// always @(*) begin
+//     data_out = register_32_bits;
+// end
+    
+endmodule
